549cf18bfe074ee77feb7553fd50c8f5df746de5
Calvin Godfrey <calvinbbcard@gmail.com>
2017-07-09 22:13:25 -0400

    Create script.js

---
+++ DiffEq/script.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+  var canvas = document.getElementById("canvas");
+  canvas.width = screen.width;
+  canvas.height = screen.height;
+  var height = canvas.height;
+  var width = canvas.width;
+  var ctx = canvas.getContext("2d");
+  var space = 75;
+  
+  function main(){
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#FFF";
+    var i = 0;
+    while(i < width){
+      ctx.fillRect(i, 0, i+10, height);
+      i += space;
+    }
+    i = height;
+    while(i > height){
+      ctx.filleRect(0, i, width, i+10);
+    }
+    requestAnimationFrame(main);
+  }
+  requestAnimationFrame(main);
+}
