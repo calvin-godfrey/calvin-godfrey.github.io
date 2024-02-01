d7fe3ea9c3ddb0026f15a0b41dbc519a612c23b7
Calvin Godfrey <calvinbbcard@gmail.com>
2024-01-31 22:21:14 -0500

    Updated styles

--- LaughTrack/bundle.js
+++ LaughTrack/bundle.js
@@ -199,14 +199,16 @@ client.connect();
 // LOAD AUDIO FILES
 const audio_files = [new Audio('media/laugh1.mp3'), new Audio('media/laugh2.mp3'), new Audio('media/laugh3.mp3'), new Audio('media/laugh4.mp3'), new Audio('media/laugh5.mp3'), new Audio('media/laugh6.mp3')];
 let last_laugh_time = new Date() / 1000;
-const LAUGH_THRESHOLD = 2;
+const LAUGH_THRESHOLD = 6;
 const LAUGH_COOLDOWN_IN_SECONDS = 15;
 let current_laugh_count = 0;
 
 client.on('message', (channel, tags, message, self) => {
     message = message.trim();
-    console.log(`${tags['display-name']}: ${message}`);
-    console.log(`${current_laugh_count}`);
+
+    // UPDATE CURRENT MESSAGE.
+    document.getElementById("chat").innerHTML = `${tags['display-name']}: ${message}`;
+
     let is_icant = true;
     words = message.split(" ");
     for (let i = 0; i < words.length; ++i)
