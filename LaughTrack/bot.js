d8b703946c0bbd60ce269cd3baa2b40aac955956
Calvin Godfrey <calvinbbcard@gmail.com>
2024-02-03 18:05:08 -0500

    Crickets

--- LaughTrack/bot.js
+++ LaughTrack/bot.js
@@ -9,10 +9,11 @@ client.connect();
 
 // LOAD AUDIO FILES
 const audio_files = [new Audio('media/laugh1.mp3'), new Audio('media/laugh2.mp3'), new Audio('media/laugh3.mp3'), new Audio('media/laugh4.mp3'), new Audio('media/laugh5.mp3'), new Audio('media/laugh6.mp3')];
+const crickets = new Audio('media/cricket.mp3');
 let last_laugh_time = new Date() / 1000;
 const LAUGH_COOLDOWN_IN_SECONDS = 15;
 
-const ICANT_THRESHOLD = 5;
+const THRESHOLD = 5;
 const MESSAGES_KEPT = 10;
 let recent_messages = [];
 
@@ -22,22 +23,29 @@ client.on('message', (channel, tags, message, self) => {
     // UPDATE CURRENT MESSAGE.
     document.getElementById("chat").innerHTML = `${tags['display-name']}: ${message}`;
 
+    // UPDATE RECENT MESSAGES.
     recent_messages.push(message);
     if (recent_messages.length > MESSAGES_KEPT)
     {
       recent_messages.shift();
     }
 
+    // CHECK FOR SOUND EFFECTS.
     let icant_count = 0;
+    let ican_count = 0;
     for (let i = 0; i < recent_messages.length; ++i)
     {
       if (recent_messages[i].includes("ICANT"))
       {
         icant_count += 1;
       }
+      else if (recent_messages[i].includes("ICAN"))
+      {
+        ican_count += 1;
+      }
     }
 
-    if (icant_count > ICANT_THRESHOLD)
+    if (icant_count > THRESHOLD)
     {
       let current_time = new Date() / 1000;
       if (current_time - last_laugh_time > LAUGH_THRESHOLD)
@@ -46,5 +54,14 @@ client.on('message', (channel, tags, message, self) => {
           const audio = audio_files[Math.floor(Math.random() * audio_files.length)];
           audio.play();
       }
+    }
+    else if (ican_count > THRESHOLD)
+    {
+        let current_time = new Date() / 1000;
+        if (current_time - last_laugh_time > LAUGH_THRESHOLD)
+        {
+            last_laugh_time = current_time;
+            crickets.play();
+        }
     }
 });
